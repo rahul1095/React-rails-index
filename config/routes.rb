3d50ab53bdae187644b94b406e3cf1d9b002d495
Rahul jain <jain.rahul1095@gmail.com>
2019-01-20 20:45:04 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+
+
+root to: 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmln
+
+  namespace :api do
+  	namespace :v1 do
+  	 resources :fruits, only: [:index, :create, :destroy, :update]
+  	end
+  end
+end
+
+
+
+
+# namespace :api do 
+#     namespace :v1 do 
+#      resources :fruits, only: [:index, :create, :destroy, :update]
+#     end 
+#   end 
